a1f1c19eed1480e840f1b5fd038a58993762309a
gughan7 <52011574+gughan7@users.noreply.github.com>
2024-01-12 16:17:37 +0530

    Add files via upload

---
+++ cogs/tours.py
@@ -0,0 +1,86 @@
+# Create a new file called cog_example.py
+# Import the necessary modules
+import typing
+import discord
+from discord.ext import commands
+from discord import app_commands
+import traceback
+# Create a class for your cog
+import asyncio
+import gspread
+import requests
+
+
+class Tours(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+    self.sheet_key = '1E8fmroZgGrTYWs4kIdzLFd5y-k5P14dRWLQ13xYz7Rw'
+    sheet_credential = gspread.service_account('keys.json')
+    self.spreadsheet = sheet_credential.open_by_key(self.sheet_key)
+    worksheet = self.spreadsheet.worksheet('team_info')
+    self.playing_teams = worksheet.col_values(1)
+    print(self.playing_teams)
+
+  @commands.hybrid_command(name='refresh',
+                           description="Refresh the team info data")
+  async def refresh(self, ctx):
+    await ctx.defer()
+    self.sheet_key = '1E8fmroZgGrTYWs4kIdzLFd5y-k5P14dRWLQ13xYz7Rw'
+    sheet_credential = gspread.service_account('keys.json')
+    self.spreadsheet = sheet_credential.open_by_key(self.sheet_key)
+    worksheet = self.spreadsheet.worksheet('team_info')
+    self.playing_teams = worksheet.col_values(1)
+    print(self.playing_teams)
+    await ctx.send("Data Refreshed ! ")
+
+  @commands.command()
+  async def syncs(self, ctx):
+    self.bot.tree.copy_global_to(guild=ctx.guild)
+    await self.bot.tree.sync()
+    await self.bot.tree.sync(guild=ctx.guild)
+    await ctx.reply("```Commands synced to the server ! ```")
+
+  async def team_detail_autocompletion(
+      self, ctx: commands.Context,
+      current: str) -> typing.List[app_commands.Choice[str]]:
+    data = [
+        app_commands.Choice(name=team, value=team)
+        for team in self.playing_teams
+        if team.lower().startswith(current.lower())
+    ]
+    data = data[:24]
+    return data
+
+  @commands.hybrid_command(name="team_info",
+                           description="Load the tour player info")
+  @app_commands.autocomplete(team_name=team_detail_autocompletion)
+  async def challonge_tournament(self, ctx, team_name: str):
+    await ctx.defer()
+    worksheet = self.spreadsheet.worksheet('team_info')
+    k = worksheet.find(team_name)
+    s = worksheet.row_values(int(k.row))
+    col_name = worksheet.row_values(1)
+    embed = discord.Embed(
+        title=f"**{team_name}**",
+        description=f"Here you can see all the info of the **{team_name}** team"
+    )
+    for i, j in enumerate(s):
+      if i > 1:
+        if j.isnumeric():
+          j = discord.utils.get(ctx.guild.members, id=int(j))
+          if j == None:
+            embed.add_field(name=f"{col_name[i]}", value=f"**{j}**")
+          else:
+            embed.add_field(name=f"{col_name[i]}",
+                            value=f"{j.name} - {j.mention}")
+        else:
+          embed.add_field(name=f"{col_name[i]}", value=f"**{j}**")
+    file = discord.File('logo.png')
+    embed.set_thumbnail(url="attachment://logo.png")
+    embed.set_footer(text="Created & Managed by **ECOW**")
+    await ctx.send(embed=embed)
+
+
+async def setup(bot):
+  await bot.add_cog(Tours(bot))
---
+++ main.py
@@ -0,0 +1,39 @@
+#from logging import RootLogger
+from discord import interactions
+from discord.flags import Intents
+import discord
+from discord.ext import commands
+#from cogs.challonge import Challonge
+from cogs.tours import Tours
+#from cogs.ticket import Ticket
+#from cogs.registeration import Team_info_cog
+from discord import app_commands
+from keep_alive import keep_alive
+import typing
+import sqlite3
+import os
+
+keep_alive()
+
+
+def run():
+  intents = discord.Intents.all()
+  bot = commands.Bot(command_prefix="?", intents=intents)
+  bot.remove_command("help")
+
+  @bot.event
+  async def on_ready():
+    print("_______________________________")
+
+    print(await bot.load_extension('cogs.tours'))
+    #print(await bot.load_extension('cogs.challonge'))
+    #print(await bot.load_extension('cogs.ticket'))
+    #print(await bot.load_extension('cogs.registeration'))
+    activity = discord.Game(name="Hand-Crafted by R")
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+
+  bot.run(token=os.environ['token'])
+
+
+if __name__ == "__main__":
+  run()
